49477274a7efdeb1c344b38cd159b844ba53dd42
ArtHard <1artem1nazarov1@gmail.com>
2025-01-22 20:03:00 +0300

    create slider in intro

---
+++ mlbbinfo/maininfo/static/maininfo/js/main-slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".intro-slider", {
+	loop: true,
+	allowTouchMove: false,
+	effect: "fade",
+	fadeEffect: {
+		crossFade: true,
+	},
+	autoplay: {
+		delay: 5000,
+	},
+});
